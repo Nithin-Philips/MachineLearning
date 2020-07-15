68e35d954bf1f50e8b14a9099870dfaef7b3eff9
NITHIN GEORGE PHILIPS <nphilips914@gmail.com>
2020-07-15 12:16:58 +0530

    dnjsd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
-from tensorflow.python.keras.preprocessing import image
-from tensorflow.python.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 
 from flask import Flask,request
 import os

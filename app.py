3068e145f91b39124d7ad0ff18160f354a1751f2
NITHIN GEORGE PHILIPS <nphilips914@gmail.com>
2020-07-15 13:28:19 +0530

    dkmd

--- app.py
+++ app.py
@@ -9,23 +9,24 @@ model = load_model("saved_model.h5")
 from PIL import Image
 
 
-@app.route('/')
-def home():
-    return "hello there"
 
-@app.route("/api",methods=["POST"])
+
+@app.route("/",methods=["GET","POST"])
 def api():
-    img = request.files['image'].stream
-    im = Image.open(img)
-    im = im.save('image.jpeg')
-    test_image = image.load_img('image.jpeg',target_size=(64,64)) 
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis=0)
-    result = model.predict(test_image)
-    if result[0][0]== 1 :
-        prediction = 'PNEUMONIA'
+    if request.method == "GET":
+        return "<h1>Send image as a post request</h1>"
     else:
-        prediction = 'NORMAL'
+        img = request.files['image'].stream
+        im = Image.open(img)
+        im = im.save('image.jpeg')
+        test_image = image.load_img('image.jpeg',target_size=(64,64)) 
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = model.predict(test_image)
+        if result[0][0]== 1 :
+            prediction = 'PNEUMONIA'
+        else:
+            prediction = 'NORMAL'
     os.remove("image.jpeg")
     return prediction
 
